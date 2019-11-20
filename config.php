441ca6abd89c0e9be4d6ad49e3c7b317eb7bceb2
YPPIIBatu <57980141+YPPIIBatu@users.noreply.github.com>
2019-11-20 14:37:22 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$channelAccessToken = 'CHANNEL ACCESS TOKEN';
+$channelAccessToken = '/4n3kmjLm2OVjUzk9OQhU2ghd7mI4aDNJsf2C/ATJBDL6mXvh9pqXwVkH7PGUIgVy9eN0nWZhqhd4avb+4U+wgDlpT0XGDau0yD2GClYmgRhvz3Pa8qfUf8k1HiQZyC2qn4N/yZPSqn3m315Z+xHqQdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = 'CHANNEL SECRET';
+$channelSecret = '1616fb7472e86d26c92173f488245344';
